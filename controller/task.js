7632f6abaed9b8fe7bb3b0f6de852dff7dd23110
Arnav Kumar Gupta <arnavkumargupta777@gmail.com>
2025-05-11 17:45:10 +0530

    Add validation and handle

--- controller/task.js
+++ controller/task.js
@@ -4,8 +4,12 @@ const getAllTasks = (req, res) => {
 };
 
 const createtask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json({ task });
+  try {
+    const task = await Task.create(req.body);
+    return res.status(201).json({ task });
+  } catch (error) {
+    return res.status(400).send(error.message);
+  }
 };
 const getTask = (req, res) => {
   res.json({ id: req.params.id });
