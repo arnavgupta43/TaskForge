442f52a42a8f73bfd8ea94a224aef28595c004fa
Arnav Kumar Gupta <arnavkumargupta777@gmail.com>
2025-05-11 17:25:46 +0530

    Add post request

--- controller/task.js
+++ controller/task.js
@@ -1,9 +1,11 @@
+const Task = require("../models/Task");
 const getAllTasks = (req, res) => {
   res.send("all items from the file");
 };
 
-const createtask = (req, res) => {
-  res.json(req.body);
+const createtask = async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json({ task });
 };
 const getTask = (req, res) => {
   res.json({ id: req.params.id });
